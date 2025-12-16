5ddff8d2707445338e7959914a6819dfb01f8e55
Nurzhan <nurzhqn@gmail.com>
2025-12-16 19:16:57 +0500

    feat: add dotenv

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+
 const PORT = 3000;
+
+const COUNTRY_LAYER_API = process.env.COUNTRY_LAYER_API;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
