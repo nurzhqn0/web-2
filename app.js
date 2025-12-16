84cb4142cd9ccb7cb346170a5fa3bbfe851a79c8
Nurzhan <nurzhqn@gmail.com>
2025-12-16 19:12:46 +0500

    feat: add boilerplate

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
